0d5698aee392b01f95368828a08f6124ee030951
Idan Raiter <31453424+iraiter2@users.noreply.github.com>
2020-05-12 18:53:46 -0500

    Remove legacy code

--- permissions.py
+++ permissions.py
@@ -11,20 +11,5 @@ def bit_on(perm, i):
 def has(user, permission):
     return bit_on(user.permissions, permission.value)
 
-def can_say(user):
-    return has(user, Permissions.member)
-
-def can_play(user):
-    return has(user, Permissions.admin)
-
-def can_create_category(user):
-    return has(user, Permissions.admin)
-
-def can_transact(user):
-    return has(user, Permissions.member)
-
-def can_edit_protected_category(user):
-    return has(user, Permissions.admin)
-
 def get_members():
     return [{"id": x["id"], "displayname": x["displayname"]} for x in get_db()["users"].find(permissions={">": 0})]
